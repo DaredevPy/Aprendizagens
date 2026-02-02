86c625fe6c5d2d02b14bb7c0511fd49d5a3598b1
DaredevPy <renatopiment@gmail.com>
2026-02-02 18:29:09 -0300

    Declarado todas as variaveis

---
+++ .vscode/Gerenciamento_Produtos.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    char produto_a[30] = "Produto A";
+    char produto_b[30] = "Produto B";
+
+    unsigned int estoque_a = 1000;
+    unsigned int estoque_b = 2000;
+
+    float valor_a = 10.00;
+    float valor_b = 20.00;
+
+    unsigned int estoqueminimo_a = 500;
+    unsigned int estoqueminimo_b = 2500;
+
+    double valor_total_a;
+    double valor_total_b;
+
+  
+    }
