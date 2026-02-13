af09bbe2650b5812486aba1a18cf6c63049eb5ac
DaredevPy <renatopiment@gmail.com>
2026-02-13 14:47:05 +0000

    Vs_02

--- Desafio_If_else.c
+++ Desafio_If_else.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 int main()
-float temperatura, umidade;
-unsigned int estoque estoque_min = 1000;
+{
+    float temperatura, umidade;
+    unsigned int estoque, estoque_min = 1000;
 
 printf("Entre com o valor da temperatura: \n");
 scanf("%f", &temperatura);
@@ -13,18 +14,19 @@ scanf("%u", &estoque);
 
 if (temperatura > 30){
     printf("A temperatura esta acima de 30 graus\n");
-} {else 
+} else {
     printf("A temperatura esta dentro do intervalo de 30 graus\n");
 }
 
 if (umidade > 50){
     printf("A umidade esta acima de 50 porcento\n");
-} {else 
+} else {
     printf("A umidade esta dentro do intervalo de 50 porcento\n");
 }
 
 if (estoque < estoque_min){
-    printf("O estoque esta abaixo do valor mínimo! \n");
-} {else 
+    printf("O estoque esta abaixo do valor mínimo\n");
+} else {
     printf("O estoque esta normal\n");
+}
 }
