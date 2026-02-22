98e398036e81e9865d2a1c44712a3a1ffd335f07
DaredevPy <renatopiment@gmail.com>
2026-02-22 14:59:49 +0000

    Retorna tabuada do numero digitado

---
+++ Tabuada_FOR.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int numero, i;
+    printf("Digite um numero para ver a tabuada: ");
+    scanf("%d", &numero);
+
+    for (i = 1; i <= 10; i++)
+    {
+        printf("%d x %d = %d\n", numero, i, numero * i);
+    }
+    return 0;
+}
