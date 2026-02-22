2dcc651638aa6672825c6cbec9c0b206c3ad4eb9
DaredevPy <renatopiment@gmail.com>
2026-02-22 15:56:08 +0000

    VS_1.1

--- xadrez.c
+++ xadrez.c
@@ -3,18 +3,18 @@
 int main() {
     
     // Declarando variaveis para o número de casas que cada peça irá mover
-    
-    int casas_torre = 5;      // Número de casas que a Torre irá mover
-    int casas_bispo = 5;      // Número de casas que o Bispo irá mover
-    int casas_rainha = 8;     // Número de casas que a Rainha irá mover
-    int i;                    // Variável de controle para loops
+    int i; // Variável para controlar os loops
+    int casas_torre = 5;      // Quantidade de casas que a Torre irá mover
+    int casas_rainha = 8;     // Quantidade de casas que a Rainha irá mover
+    int casas_bispo = 5;      // Quantidade de casas que o Bispo irá mover
+                      
     
  
     // MOVIMENTO DA TORRE
     printf("\n*** MOVIMENTO DA TORRE ***\n");
     printf("Movendo 5 casas para a FRENTE:\n");
     
-    // Utilizando for
+    // __________________Utilizando for_________________
     for(i = 1; i <= casas_torre; i++) {
         printf(" %d: Frente\n", i);
     }
@@ -25,7 +25,7 @@ int main() {
     printf("*** MOVIMENTO DO BISPO ***\n");
     printf("Movendo 5 casas na diagonal (CIMA e DIREITA):\n");
     
-    // Utilizando while
+    //________________ Utilizando while_________________
     i = 1;  // Reinicializando o contador
     while(i <= casas_bispo) {
         printf(" %d: Cima, Direita\n", i);
@@ -38,14 +38,13 @@ int main() {
     printf("*** MOVIMENTO DA RAINHA ***\n");
     printf("Movendo 8 casas para a ESQUERDA:\n");
     
-    // Utilizando do while
+    // ___________________Utilizando do while_____________________
     i = 1;  // Reinicializando o contador
     do {
         printf(" %d: Esquerda\n", i);
         i++;  // Incrementa o contador
     } while(i <= casas_rainha);
     printf("Rainha moveu %d casas para a esquerda!\n\n", casas_rainha);
-    
     
     return 0;
 }
