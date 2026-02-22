c1892c01d806158d85d37d733a128b0859709844
DaredevPy <renatopiment@gmail.com>
2026-02-22 14:03:51 +0000

    comentario

--- Imprimir_par_FOR.c
+++ Imprimir_par_FOR.c
@@ -5,7 +5,7 @@ int main() {
 
 int i = 0;
 while (i <= 10){
-    if(i % 2 == 0){
+    if(i % 2 == 0){  //(i % 2 !== 0) para imprimir os números ímpares
         printf("O numero é: %d é par!\n", i);
     }
     i++;
