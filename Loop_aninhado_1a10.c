df2435ade1633418205ed9164d4eb64ad15e8094
DaredevPy <renatopiment@gmail.com>
2026-02-23 22:39:18 +0000

    Vs_01

--- Loop_aninhado_1a10.c
+++ Loop_aninhado_1a10.c
@@ -3,7 +3,7 @@
 int main() {
     for (int i = 1; i <= 10; i++) {
         for (int j = 1; j <= 10; j++) {
-            printf("%d\t", i * j);
+            printf("%d \n ", i * j);
         }
         printf("\n");
     }
