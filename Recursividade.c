8f130f48e1f13c8bbbbdfeaa0f089deebd4d0f9f
DaredevPy <renatopiment@gmail.com>
2026-03-02 23:17:08 +0000

    Inverção da contagen

--- Recursividade.c
+++ Recursividade.c
@@ -2,9 +2,9 @@
 
 void recursivo (int numero) {
     if (numero > 0) {
-        printf("%d \n ", numero);
         
-        recursivo(numero - 1);
+        printf("%d \n ", numero);
+        recursivo(numero - 1); // Colocando i printf depois da chamada recursiva, a contagem será crescente
     }
 }
 int main () {
