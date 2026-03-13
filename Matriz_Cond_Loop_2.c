7d37113c8c7944e7b7e217db72eb4542258b41dd
DaredevPy <renatopiment@gmail.com>
2026-03-13 14:28:37 +0000

    #define LINHAS  5 #define COLUNAS 5 Podemos escolher o tamanho da matriz

--- Matriz_Cond_Loop_2.c
+++ Matriz_Cond_Loop_2.c
@@ -4,10 +4,22 @@
 #define COLUNAS 5
  
 int main() {
-    int matriz[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}; // Matriz 3x3 inicializada com valores definidos
+    
+    int matriz[LINHAS][COLUNAS];
     int target = 5; // Variavel para armazenar o elemento a ser buscado
     int found = 0; // Variavel para indicar se o elemento foi encontrado ou não
- 
+    int soma = 1; // Variavel para armazenar a soma dos elementos da matriz
+
+     for (int i = 0; i < LINHAS; i++) {      // Loop externo para as linhas
+        for (int j = 0; j < COLUNAS; j++) {
+            matriz[i][j] = soma;   // Loop interno para as colunas
+            soma ++; // Incrementa a soma para o próximo elemento
+            printf("%d ", matriz[i][j]); // Imprime o elemento da matriz    
+        }
+        printf("\n"); // Imprime uma nova linha após cada linha da matriz
+    }
+    
+
     // Busca condicional do elemento alvo
     for (int i = 0; i < LINHAS; i++) {      // Loop externo para as linhas
         for (int j = 0; j < COLUNAS; j++) {  // Loop interno para as colunas
